c92216429419cf812bfbb6f01917e2e779e2dfe2
drilja <ilja@mirsky.eu>
2021-02-06 19:52:40 +0100

    changing object

--- gesture-handler.js
+++ gesture-handler.js
@@ -4,8 +4,8 @@ AFRAME.registerComponent("gesture-handler", {
     schema: {
       enabled: { default: true },
       rotationFactor: { default: 5 },
-      minScale: { default: 0.3 },
-      maxScale: { default: 8 },
+      minScale: { default: 0.1 },
+      maxScale: { default: 10 },
     },
   
     init: function () {
